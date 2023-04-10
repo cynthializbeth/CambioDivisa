dc6dafa23aa21ef837376f0038c093a84b25718e
Cynthia Sánchez <cyndi_cieusagi@ciencias.unam.mx>
2023-04-10 04:11:46 -0600

    cambios rate de String a Double y rate sale como unico campo de la respuesta

--- src/main/java/com/exchangerate/api/controller/CtrlExchangeRate.java
+++ src/main/java/com/exchangerate/api/controller/CtrlExchangeRate.java
@@ -11,24 +11,23 @@ import com.exchangerate.api.entity.ExchangeRateSimple;
 @RestController
 public class CtrlExchangeRate {
     
-    @GetMapping("/rate/{id}")
-    public ExchangeRateSimple getExchangeRate(@PathVariable String id) {
+    @GetMapping("/rate/{currency}")
+    public ExchangeRateSimple getExchangeRate(@PathVariable String currency) {
         // Crea una instancia de RestTemplate
         RestTemplate restTemplate = new RestTemplate();
     
         // Realiza la solicitud GET a la API CoinCap y obtiene la respuesta
-        String url = "https://api.coincap.io/v2/rates/" + id;
+        String url = "https://api.coincap.io/v2/rates/" + currency;
         ExchangeRate response = restTemplate.getForObject(url, ExchangeRate.class);
     
         // Verifica si la respuesta es nula
         if (response == null || response.getData() == null) {
-            throw new ApiException("Divisa no encontrada: " + id);
+            throw new ApiException("Divisa no encontrada: " + currency);
         }
     
         // Crea una instancia de ExchangeRateSimple y asigna los valores correspondientes
         ExchangeRateSimple simpleResponse = new ExchangeRateSimple();
-        simpleResponse.setCurrency(response.getData().getId());
-        simpleResponse.setRate(response.getData().getRateUsd());
+        simpleResponse.setRate(Double.parseDouble(response.getData().getRateUsd()));
     
         // Retorna la respuesta simplificada de la API CoinCap al cliente
         return simpleResponse;
--- src/main/java/com/exchangerate/api/entity/ExchangeRateSimple.java
+++ src/main/java/com/exchangerate/api/entity/ExchangeRateSimple.java
@@ -3,26 +3,16 @@ package com.exchangerate.api.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ExchangeRateSimple {
-    @JsonProperty("currency")
-    private String currency;
     @JsonProperty("rate")
-    private String rate;
+    private Double rate;
 
     public ExchangeRateSimple() {}
 
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getRate() {
+    public Double getRate() {
         return rate;
     }
 
-    public void setRate(String rate) {
+    public void setRate(Double rate) {
         this.rate = rate;
     }
     
